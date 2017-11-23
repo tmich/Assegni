60ee5be01e2f9830898e4d86b4d7697f77d86aac
tiziano <tiziano@DESKTOP-Q5QC1HN>
2017-11-23 20:13:57 +0100

    Ricerca dal 2016

--- cmbanni.cpp
+++ cmbanni.cpp
@@ -25,7 +25,7 @@ void CComboAnni::OnInitialUpdate()
 {
 	dtm::date now;
 	int y = now.year();
-	while (y >= 2017)
+	while (y >= 2016)
 	{
 		int i = AddString(std::to_wstring(y).c_str());
 		SetItemData(i, DWORD(y));
